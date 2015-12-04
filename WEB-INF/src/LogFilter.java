448feb1a52cd4e2aea73c4205168f19a67656954
jun <785691557@qq.com>
2015-12-05 00:07:17 +0800

    logFilter

--- WEB-INF/src/LogFilter.java
+++ WEB-INF/src/LogFilter.java
@@ -1,5 +1,12 @@
 package com.jun.filter;
+
+import javax.servlet.Filter;
 import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+import java.io.*;
+
 @WebFilter(filterName="login",
 			urlPatterns={"/*"})
 public class LogFilter implements Filter{
@@ -12,5 +19,17 @@ public class LogFilter implements Filter{
 		this.config=null;
 	}
 	
+	public void doFilter(ServletRequest request,ServletResponse response,FilterChain chain)
+		throws IOException,ServletException{
+			ServletContext context = this.config.getServletContext();
+			long before = System.currentTimeMillis();
+			System.out.println("开始过滤");
+			HttpServletRequest hrequest  = (HttpServletRequest)request;
+			System.out.println("Filter已经截获到用户的请求的地址:"+hrequest.getServletPath());
+			chain.doFilter(request,response);
+			long after = System.currentTimeMillis();
+			System.out.println("过滤结束");
+			System.out.println("请求被定位到："+hrequest.getRequestURI()+"所花的时间："+(after-before));
+	}
 	
 }
---
+++ login.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=GBK" language="java" errorPage=""%>
+<html>
+	<head>
+		<title>logFilter</title>
+	</head>
+	<body>
+		Welcome
+	</body>
+</html>
